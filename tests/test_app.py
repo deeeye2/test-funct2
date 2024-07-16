be04a6ec8df24a19020446a4da3f92d2b12bb513
MOHAMED SESAY <162488876+deeeye2@users.noreply.github.com>
2024-07-16 10:39:09 +0200

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+import unittest
+from app import app
+
+class FlaskTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_index### Project Structure
+
